ef7c3c0db84d650c255a48bb70581680a7fff1b5
daniloax <danilo.alvesx@gmail.com>
2015-05-18 12:37:41 -0300

    update files

--- 20151/ps/semana08/lab/splint/splint.c
+++ 20151/ps/semana08/lab/splint/splint.c
@@ -1,27 +1,18 @@
 #include<stdio.h>
-
-int multiplica ( int x, int *y, /*@out@*/ int *resultado) {
-   
-   int i, produto;
-   
-   while(i < y) {
-      produto += x;
-   }
-   
-   return produto;
-
+void multiplica ( int x, int *y, int *resultado ){
+	int i, produto;	
+	while( i < y){
+		produto += x;
+	}
+	return produto;
 }
-
+	
 
 int main(){
-
-   int x, *y, resultado;
-   
-   x = 4;
-   *y = 3;
-   multiplica(x, y, &resultado);
-   printf ( "%d", resultado );
-   
-   return 0;
+	int x, *y, resultado;
+	x = 4;
+	*y = 3;
+	multiplica( x, y, &resultado);
+	printf ( "%d", resultado );
 
 }
